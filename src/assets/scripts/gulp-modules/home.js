365d75683910af59660cc437b19aba465205abb0
Dron2019 <mr.andrey@bigmir.net>
2021-12-09 13:15:40 +0200

    slider add

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -25,7 +25,7 @@ const swiper = new Swiper('.zoom-slider-wrapper', {
   },
 });
 
-swiper.on('sliderFirstMove', () => {
+swiper.on('touchStart', () => {
   gsap.to('.zoom-slider-wrapper', { scale: 0.95, duration: 0.5, ease: 'power4.out' });
 
 })
@@ -41,6 +41,20 @@ gsap.defaults({
   duration: 2.5,
 })
 gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger: '.screen4',
+    scrub: true,
+    // start: '0 top',
+    pin: '.screen4__inner',
+    markers: true
+  }
+})
+  .from('.screen4__title :first-child', { x: '-50vw' })
+  .from('.screen4__title :last-child', { x: '50vw' }, '<')
+
 
 // headerHandle();
 
