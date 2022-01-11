90904134e9f7e74b04e04ef9d3dc1a82cecd200e
unknown <mr.andrey@bigmir.net>
2022-01-11 11:54:11 +0200

    menu adapt

--- src/assets/scripts/modules/menu/menu.js
+++ src/assets/scripts/modules/menu/menu.js
@@ -31,27 +31,35 @@ function openMenu(button, menu) {
     button.addEventListener('click',function(evt){
         gsap.timeline({
             duration: 2.5
-        })
+        }) 
             .add(enableCurtainFromBottom(), '<')
             .to(menu, { autoAlpha: 1, duration: 0.25 }, '<+1.5')
             .fromTo(menuLinks, 
                 { y: 50, autoAlpha: 0, }, 
                 { y: 0, autoAlpha: 1, clearProps: 'all', duration: 1.25 }, 
                 '<')
+            .add(() => {
+                document.body.classList.add('popup-opened');
+            })
     });
 }
+
 function closeMenu(button, menu) {
     const menuLinks = menu.querySelectorAll('.menu__main [data-menu-links]>*');
     button.addEventListener('click',function(evt){
         gsap.timeline({
             duration: 2.5
         })
+        
         .add(enableCurtainFromTop(), '<')
         .to(menu, { autoAlpha: 0, duration: 0.25 }, '<+1.5')
         .fromTo(menuLinks, 
             { y: 0, autoAlpha: 1, }, 
             { y: 50, autoAlpha: 0, duration: 0.75 }, 
             '<-1')
+        .add(() => {
+            document.body.classList.remove('popup-opened');
+        })
     });
 }
 function enableCurtainFromBottom() {
