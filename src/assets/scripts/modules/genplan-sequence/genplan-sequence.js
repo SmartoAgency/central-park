78777092dc1668af8cf5c35f94c1ea552e7c3919
Dron2019 <dron_2019@ukr.net>
2022-03-24 18:37:57 +0200

    add genplan with actual sequences

--- src/assets/scripts/modules/genplan-sequence/genplan-sequence.js
+++ src/assets/scripts/modules/genplan-sequence/genplan-sequence.js
@@ -12,15 +12,34 @@ export default async function genplanSequence(config) {
             console.log(e);
         }
     });
+    let loadedSequences = {
+
+    };
+    let activeSequence;
+    const clickSequences = {
+        areas_1: 'genplan_1.json',
+        areas_2: 'genplan_2.json',
+        areas_finals:'genplan_3.json',
+    };
+
+    Object.entries(clickSequences).forEach(async ([key, val]) => {
+        let URL = window.location.href.match(/localhost/) ? './static/'+val : '/wp-content/themes/central-park/static/'+val;
+        let data = await axios(URL, {
+            onDownloadProgress: (e) => {
+                console.log(e);
+            }
+        });
+        clickSequences[key] = data.data;
+    })
     // SEQUENCES = await SEQUENCES.json();
     SEQUENCES = await SEQUENCES.data;
     const sequenceLength = SEQUENCES.length;
-    console.log(SEQUENCES);
 
     ScrollTrigger.create({
         trigger: scene,
         scroller: scroller,
         end: '80% bottom',
+        onEnterBack:()=>{ activeSequence = undefined },
         onUpdate: ({progress}) => {
             console.log('upodate');
             const scaleFactor = sequenceLength / 100; 
@@ -35,36 +54,42 @@ export default async function genplanSequence(config) {
     let isAnimating = false;
     $switchFrames.forEach(frame => {
         frame.addEventListener('click',function(evt){
+            if (!clickSequences[frame.dataset.attr]) return;
+            const dataToSequence = clickSequences[frame.dataset.attr];
             if (isAnimating) return;
             isAnimating = true;
+            document.querySelector('[data-genplan-title]').textContent = frame.dataset.title;
+            if (activeSequence) {
+                changeImageSrcByArrayIndex(
+                    imgForDisplay, 
+                    clickSequences[activeSequence], 
+                    clickSequences[activeSequence].length -1, 
+                    0,
+                    () => {
+                        changeImageSrcByArrayIndex(
+                            imgForDisplay, 
+                            dataToSequence, 
+                            0, 
+                            dataToSequence.length-1,
+                            () => {
+                                activeSequence = frame.dataset.attr;
+                                isAnimating = false
+                            }
+                        );
+                    }
+                );
+                return;
+            } 
             changeImageSrcByArrayIndex(
                 imgForDisplay, 
-                SEQUENCES, 
-                30 , 
-                0,
+                dataToSequence, 
+                0, 
+                dataToSequence.length-1,
                 () => {
-                    changeImageSrcByArrayIndex(
-                        imgForDisplay, 
-                    SEQUENCES, 
-                    0 , 
-                    30,
-                        () => isAnimating = false,
-                    )
-                    // isAnimating = false
+                    activeSequence = frame.dataset.attr;
+                    isAnimating = false
                 }
             );
-            // gsap.fromTo(imgForDisplay, 
-            //     { src: SEQUENCES[0] }, 
-            //     { 
-            //         // src: SEQUENCES[sequenceLength],
-            //         onUpdate: (el) => {
-            //             console.log(el);
-            //         },
-            //         onComplete:() => {
-            //             isAnimating = false;
-            //         } 
-            //     }
-            // )
         });
     })
 }
@@ -73,7 +98,7 @@ export default async function genplanSequence(config) {
 
 
 function changeImageSrcByArrayIndex(toDisplay, images, start, end, cb = () => {}) {
-    const delay =  1000 / 60;
+    const delay =  1000 / 25;
     function change(i) {
         toDisplay.src = images[i];
         if (i === end) return cb();
@@ -83,13 +108,6 @@ function changeImageSrcByArrayIndex(toDisplay, images, start, end, cb = () => {}
     }
     change(start);
 }
-const sequenceIndexes = [
-    '0~99',
-    '100~119',
-    '120~139',
-    '140~149',
-    '150~169',
-];
 function getElementBySelector(arg) {
     if (typeof arg === 'string') return document.querySelector(arg);
     return arg;
