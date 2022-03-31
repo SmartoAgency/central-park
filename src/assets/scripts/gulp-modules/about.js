64de7315c77efd9bf5bc94bf785aca3402ec636b
unknown <mr.andrey@bigmir.net>
2022-03-31 18:18:32 +0300

    add anim to mobile aboput and adv pages

--- src/assets/scripts/gulp-modules/about.js
+++ src/assets/scripts/gulp-modules/about.js
@@ -4,6 +4,7 @@ import { handleHeader } from "../modules/helpers/helpers";
 import curtainsShaders from "../modules/curtains-shaders";
 import splitToLinesAndFadeUp from "../modules/effects/splitToLinesAndFadeUp";
 import clipPathEntry from "../modules/effects/clipPathEntry";
+import fadeInUp from "../modules/effects/fadeInUp";
 
 innerPageFrontEffect();
 window.addEventListener('load',function(evt){
@@ -14,6 +15,8 @@ window.addEventListener('load',function(evt){
     window.scroller = scroller;
     !isMobile && curtainsShaders(scroller);
     splitToLinesAndFadeUp('.about-text, .about-text-l, .about-text-xl, .about-pretty-block__text p','.scroller-container');
+    isMobile && fadeInUp('.about-character-part', document.body);
+    isMobile && splitToLinesAndFadeUp('.about-mob-block-gradient__title,.about-mob-block-gradient__text,.about-mob-title', document.body);
     clipPathEntry('.about-pretty-block__decor path','.scroller-container', {
       stagger: 0.15
     })
