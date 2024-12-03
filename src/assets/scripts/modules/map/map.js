7b5249989242ec1853a1835485f705900057c286
AndriiSolodaiev <andrii.solodaiev@gmail.com>
2024-12-03 10:37:58 +0200

    add Api key, and positioning img "building progress"

--- src/assets/scripts/modules/map/map.js
+++ src/assets/scripts/modules/map/map.js
@@ -1,14 +1,12 @@
-import mapStyle from "./map-style";
-
-
+import mapStyle from './map-style';
 
 export default function googleMap() {
-  global.initMap = initMap
+  global.initMap = initMap;
 }
 
 async function func() {
   const script = document.createElement('script');
-  let key = '';
+  let key = 'AIzaSyCaYQ83EA9peTsEI6ih6cUc6uaCjn080qE';
   if (window.location.href.match(/localhost/)) key = '';
   // const key = '';
   script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`;
@@ -21,10 +19,10 @@ const options = {
   threshold: 0.1,
 };
 
-maps.forEach((image) => {
+maps.forEach(image => {
   const callback = (entries, observer) => {
     /* Content excerpted, show below */
-    entries.forEach((entry) => {
+    entries.forEach(entry => {
       if (entry.isIntersecting) {
         const lazyImage = entry.target;
         lazyImage.src = lazyImage.dataset.src;
@@ -60,43 +58,43 @@ function initMap() {
     scaleControl: false,
     draggable: true,
     language: 'en',
-    styles: mapStyle()
+    styles: mapStyle(),
   });
   window.googleMap = map;
   var polygonCoords = [
-    new google.maps.LatLng(49.2384203, 28.4600074,),
-    new google.maps.LatLng(49.2385850, 28.4598870,),
-    new google.maps.LatLng(49.2391278, 28.4600104,),
-    new google.maps.LatLng(49.2391559, 28.4597287,),
-    new google.maps.LatLng(49.2395569, 28.4598146,),
-    new google.maps.LatLng(49.2398914, 28.4587305,),
-    new google.maps.LatLng(49.2402101, 28.4585212,),
-    new google.maps.LatLng(49.2404903, 28.4572388,),
-    new google.maps.LatLng(49.2405323, 28.4572173,),
-    new google.maps.LatLng(49.2405708, 28.4570617,),
-    new google.maps.LatLng(49.2406269, 28.4571100,),
-    new google.maps.LatLng(49.2407074, 28.4568578,),
-    new google.maps.LatLng(49.2411032, 28.4553017,),
-    new google.maps.LatLng(49.2413588, 28.4553822,),
-    new google.maps.LatLng(49.2418737, 28.4543252,),
-    new google.maps.LatLng(49.2421679, 28.4565466,),
-    new google.maps.LatLng(49.2414464, 28.4570725,),
-    new google.maps.LatLng(49.2415410, 28.4585856,),
-    new google.maps.LatLng(49.2409876, 28.4598519,),
-    new google.maps.LatLng(49.2409666, 28.4603349,),
-    new google.maps.LatLng(49.2410541, 28.4607534,),
-    new google.maps.LatLng(49.2412958, 28.4608070,),
-    new google.maps.LatLng(49.2412503, 28.4612041,),
-    new google.maps.LatLng(49.2409526, 28.4611194,),
-    new google.maps.LatLng(49.2399509, 28.4613447,),
-    new google.maps.LatLng(49.2398844, 28.4617268,),
-    new google.maps.LatLng(49.2395621, 28.4616088,),
-    new google.maps.LatLng(49.2393310, 28.4617161,),
-    new google.maps.LatLng(49.2394220, 28.4609649,),
-    new google.maps.LatLng(49.2387216, 28.4607395,),
-    new google.maps.LatLng(49.2386795, 28.4602459,),
-    new google.maps.LatLng(49.2384449, 28.4601344,),
-    new google.maps.LatLng(49.2384273, 28.4600110,),
+    new google.maps.LatLng(49.2384203, 28.4600074),
+    new google.maps.LatLng(49.238585, 28.459887),
+    new google.maps.LatLng(49.2391278, 28.4600104),
+    new google.maps.LatLng(49.2391559, 28.4597287),
+    new google.maps.LatLng(49.2395569, 28.4598146),
+    new google.maps.LatLng(49.2398914, 28.4587305),
+    new google.maps.LatLng(49.2402101, 28.4585212),
+    new google.maps.LatLng(49.2404903, 28.4572388),
+    new google.maps.LatLng(49.2405323, 28.4572173),
+    new google.maps.LatLng(49.2405708, 28.4570617),
+    new google.maps.LatLng(49.2406269, 28.45711),
+    new google.maps.LatLng(49.2407074, 28.4568578),
+    new google.maps.LatLng(49.2411032, 28.4553017),
+    new google.maps.LatLng(49.2413588, 28.4553822),
+    new google.maps.LatLng(49.2418737, 28.4543252),
+    new google.maps.LatLng(49.2421679, 28.4565466),
+    new google.maps.LatLng(49.2414464, 28.4570725),
+    new google.maps.LatLng(49.241541, 28.4585856),
+    new google.maps.LatLng(49.2409876, 28.4598519),
+    new google.maps.LatLng(49.2409666, 28.4603349),
+    new google.maps.LatLng(49.2410541, 28.4607534),
+    new google.maps.LatLng(49.2412958, 28.460807),
+    new google.maps.LatLng(49.2412503, 28.4612041),
+    new google.maps.LatLng(49.2409526, 28.4611194),
+    new google.maps.LatLng(49.2399509, 28.4613447),
+    new google.maps.LatLng(49.2398844, 28.4617268),
+    new google.maps.LatLng(49.2395621, 28.4616088),
+    new google.maps.LatLng(49.239331, 28.4617161),
+    new google.maps.LatLng(49.239422, 28.4609649),
+    new google.maps.LatLng(49.2387216, 28.4607395),
+    new google.maps.LatLng(49.2386795, 28.4602459),
+    new google.maps.LatLng(49.2384449, 28.4601344),
+    new google.maps.LatLng(49.2384273, 28.460011),
   ];
   var polygon = new google.maps.Polygon({
     path: polygonCoords, // Координаты
@@ -104,14 +102,14 @@ function initMap() {
     strokeOpacity: 0,
     // strokeWeight: 1.5,
     fillColor: '#E5CDB7',
-    fillOpacity: 1
+    fillOpacity: 1,
   });
 
   //Добавляем на карту
   polygon.setMap(map);
 
-  const filterMarkers = function (category, categoriesArray) {
-    gmarkers1.forEach((el) => {
+  const filterMarkers = function(category, categoriesArray) {
+    gmarkers1.forEach(el => {
       if (categoriesArray.has(el.category) || categoriesArray.size === 1) {
         el.setMap(map);
         el.setAnimation(google.maps.Animation.DROP);
@@ -120,8 +118,8 @@ function initMap() {
       }
     });
   };
-  filterItems.forEach((item) => {
-    item.addEventListener('click', (evt) => {
+  filterItems.forEach(item => {
+    item.addEventListener('click', evt => {
       evt.stopImmediatePropagation();
       item.classList.toggle('active');
       if (item.classList.contains('active')) {
@@ -134,7 +132,7 @@ function initMap() {
   });
 
   // var baseFolder = '/wp-content/themes/centower/assets/images/markers/';
-  const baseFolder = window.location.href.match(/localhost/) 
+  const baseFolder = window.location.href.match(/localhost/)
     ? './assets/images/markers/'
     : '/wp-content/themes/central-park/assets/images/markers/';
   let defaultMarkerSize = new google.maps.Size(40, 53);
@@ -164,7 +162,6 @@ function initMap() {
           line-height: 120%;"
           `;
 
-
   // const markersAjaxData = await fetch('')
   /* beautify preserve:start */
   async function fetchMarkersData() {
@@ -172,7 +169,7 @@ function initMap() {
     sendData.append('action', 'infrastructure');
     const url = window.location.href.match(/localhost/)
       ? 'https://central-park-wp.smarto.com.ua/wp-admin/admin-ajax.php'
-      : '/wp-admin/admin-ajax.php'
+      : '/wp-admin/admin-ajax.php';
     let markersData = await fetch(url, {
       method: 'POST',
       body: sendData,
@@ -181,26 +178,25 @@ function initMap() {
     if (!markersData) {
       console.warn('Wrong data recieved');
       return;
-    };
+    }
 
     let formatedMarkersDataForMap = markersData.reduce((acc, el) => {
       if (!el.list) return acc;
       el.list.forEach(marker => {
         acc.push({
           content: `<div ${markerPopupStyle}>${marker.name}</div>`,
-          position: { 
-            lat: marker.coordinations.latitude, 
-            lng: marker.coordinations.elevation 
+          position: {
+            lat: marker.coordinations.latitude,
+            lng: marker.coordinations.elevation,
           },
           type: el.code,
           id: marker.id,
           zIndex: 1000,
-          icon: { url: markersAdresses[el.code], scaledSize: buildLogoSize }
+          icon: { url: markersAdresses[el.code], scaledSize: buildLogoSize },
         });
       });
       return acc;
     }, []);
-
 
     console.log(formatedMarkersDataForMap);
     return formatedMarkersDataForMap;
@@ -209,7 +205,7 @@ function initMap() {
 
   ajaxMarkers.then(result => {
     putMarkersOnMap(result, map);
-  })
+  });
   console.log(ajaxMarkers);
   const markersData = [
     {
@@ -270,9 +266,9 @@ function initMap() {
       maxWidth: 200,
     });
     const initedMarkers = [];
-    markers.forEach((marker) => {
+    markers.forEach(marker => {
       const category = marker.type;
-  
+
       const mapMarker = new google.maps.Marker({
         map,
         category,
@@ -284,8 +280,8 @@ function initMap() {
       });
       mapMarker.dataId = +marker.id;
       initedMarkers.push(mapMarker);
-  
-      google.maps.event.addListener(mapMarker, 'click', function () {
+
+      google.maps.event.addListener(mapMarker, 'click', function() {
         infowindow.setContent(marker.content);
         infowindow.open(map, mapMarker);
         map.panTo(this.getPosition());
@@ -298,11 +294,7 @@ function initMap() {
     markersHandler();
   }
   /* beautify preserve:end */
-  
-  
 }
-
-
 
 function markersHightlight(google, map, infowindow) {
   const $markerLinks = document.querySelectorAll('[data-marker-id]');
@@ -310,39 +302,35 @@ function markersHightlight(google, map, infowindow) {
   //   content: '',
   //   maxWidth: 280,
   // });
-  querySelectorWithNodeList('[data-marker-id]', (item) => {
+  querySelectorWithNodeList('[data-marker-id]', item => {
     item.addEventListener('click', () => {
-
       const marker = map.initedMarkers.find(el => {
-        return el.dataId === +item.dataset.markerId
+        return el.dataId === +item.dataset.markerId;
       });
       if (marker === undefined) return;
       infowindow.setContent(marker.content);
       infowindow.open(map, marker);
       // console.log(marker);
-    })
-  })
+    });
+  });
   // console.log(document.querySelectorAll('[data-marker-id]'));
   // console.log(map);
 }
 
-
-function querySelectorWithNodeList(selector, cb = () => { }) {
+function querySelectorWithNodeList(selector, cb = () => {}) {
   const $list = document.querySelectorAll(selector);
   $list.forEach(el => cb(el));
 }
 
-
 function markersHandler() {
-  document.querySelector('.map-wrapper')
-    .addEventListener('click', ({ target }) => {
-      const map = window.googleMap;
-      if (target.closest('[data-marker-id]') === null || !map) return;
-      const markerId = target.closest('[data-marker-id]').dataset.markerId;
-      const marker = map.initedMarkers.find(marker => marker.dataId == markerId);
-      marker && map.setCenter(marker.getPosition());
-      console.log(map.initedMarkers.find(marker => marker.dataId == markerId));
-      console.log(map);
-      // console.log(marker);
-    })
+  document.querySelector('.map-wrapper').addEventListener('click', ({ target }) => {
+    const map = window.googleMap;
+    if (target.closest('[data-marker-id]') === null || !map) return;
+    const markerId = target.closest('[data-marker-id]').dataset.markerId;
+    const marker = map.initedMarkers.find(marker => marker.dataId == markerId);
+    marker && map.setCenter(marker.getPosition());
+    console.log(map.initedMarkers.find(marker => marker.dataId == markerId));
+    console.log(map);
+    // console.log(marker);
+  });
 }
