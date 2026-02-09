c24c38ec0725c664ed76f1ee305f2182e9aeda11
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-02-09 17:05:54 +0200

    fix styles on build progress block

--- src/assets/scripts/gulp-modules/home/screen3.js
+++ src/assets/scripts/gulp-modules/home/screen3.js
@@ -2,10 +2,10 @@ import gsap from 'gsap/all';
 import Swiper, { Navigation } from 'swiper';
 import { transitionBetweenSectionSceneLength } from '../../modules/helpers/helpers';
 export default function screen3Effects(scroller) {
-    const isMobile = window.matchMedia('(max-width: 575px)').matches;
-    const swiper = new Swiper('.zoom-slider-wrapper', {
-        // Optional parameters
-    modules: [ Navigation],
+  const isMobile = window.matchMedia('(max-width: 575px)').matches;
+  const swiper = new Swiper('.zoom-slider-wrapper', {
+    // Optional parameters
+    modules: [Navigation],
     slidesPerView: 4.5,
     loop: true,
     spaceBetween: 40,
@@ -13,17 +13,17 @@ export default function screen3Effects(scroller) {
       // when window width is >= 320px
       320: {
         slidesPerView: 1.5,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
       // when window width is >= 480px
       993: {
         slidesPerView: 3.5,
-        spaceBetween: 30
+        spaceBetween: 30,
       },
       1440: {
         slidesPerView: 4.5,
-        spaceBetween: 40
-      }
+        spaceBetween: 40,
+      },
       // when window width is >= 640px
     },
     navigation: {
@@ -32,14 +32,13 @@ export default function screen3Effects(scroller) {
     },
   });
   swiper.on('touchStart', () => {
-    document.querySelector('.zoom-slider-wrapper').classList.add('drag')
-  
+    document.querySelector('.zoom-slider-wrapper').classList.add('drag');
   });
   swiper.on('touchEnd', () => {
-    document.querySelector('.zoom-slider-wrapper').classList.remove('drag')
+    document.querySelector('.zoom-slider-wrapper').classList.remove('drag');
   });
   if (!isMobile) {
-    gsap.set('.screen3>*:not(.section-decor)', { y: 50, autoAlpha: 0 })
+    gsap.set('.screen3>*:not(.section-decor)', { y: 50, autoAlpha: 0 });
     gsap.timeline({
       scrollTrigger: {
         trigger: '.screen3',
@@ -47,20 +46,20 @@ export default function screen3Effects(scroller) {
         scroller: scroller ? scroller : null,
         end: '100% 20%',
         onEnter: () => {
-          gsap.to('.screen3>*:not(.section-decor)', { y: 0, autoAlpha: 1 })
+          gsap.to('.screen3>*:not(.section-decor)', { y: 0, autoAlpha: 1 });
         },
         onLeaveBack: () => {
-          gsap.to('.screen3>*:not(.section-decor)', { y: 50, autoAlpha: 0 })
+          gsap.to('.screen3>*:not(.section-decor)', { y: 50, autoAlpha: 0 });
         },
         onLeave: () => {
-          gsap.to('.screen3>*:not(.section-decor)', { y: -50, autoAlpha: 0 })
+          gsap.to('.screen3>*:not(.section-decor)', { y: -50, autoAlpha: 0 });
         },
         onEnterBack: () => {
-          gsap.to('.screen3>*:not(.section-decor)', { y: 0, autoAlpha: 1 })
-        }
-      }
-    })
+          gsap.to('.screen3>*:not(.section-decor)', { y: 0, autoAlpha: 1 });
+        },
+      },
+    });
   }
-    // .from('.screen3>*:not(.section-decor)', { scale: 1, autoAlpha: isMobile ? 0.5 : 0, y: isMobile ? 0 : 50, clearProps: 'transform' })
-    // .to('.screen2>*:not(.section-decor)', { autoAlpha: isMobile ? 0.5 : 0, y: -50 }, '<')
+  // .from('.screen3>*:not(.section-decor)', { scale: 1, autoAlpha: isMobile ? 0.5 : 0, y: isMobile ? 0 : 50, clearProps: 'transform' })
+  // .to('.screen2>*:not(.section-decor)', { autoAlpha: isMobile ? 0.5 : 0, y: -50 }, '<')
 }
